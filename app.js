aa6aaee3963abf9ab724ca9ab555326667e3bc4e
acalvom <andreac.3108@gmail.com>
2021-11-24 09:40:43 +0100

    Request: Fetch all characters

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const cards = document.querySelector("#cards");
-const cardTemplate = querySelector("#cardTemplate");
+// To ensure that the DOM has been fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+    getAllCharacters();
+});
 
+const cards = document.querySelector("#cards");
+const cardTemplate = document.querySelector("#cardTemplate");
+
+const API_URL = "http://hp-api.herokuapp.com/api/characters";
+
+const getAllCharacters = async () => {
+    try {
+        const res = await fetch(API_URL);
+        const characters = await res.json();
+        console.log(characters);
+
+    } catch (error) {
+        console.log(error);
+
+    } finally {
+        console.log('End of Request');
+    }
+}
