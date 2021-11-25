7be4af36a60d16ef008e90cbc67faa3d22026d5d
acalvom <andreac.3108@gmail.com>
2021-11-25 09:21:59 +0100

    Add information about character

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     getAllCharacters();
 });
 
-const cards = document.querySelector("#cards");
-const cardTemplate = document.querySelector("#cardTemplate");
-
-const API_URL = "http://hp-api.herokuapp.com/api/characters";
+const API_URL = "http://hp-api.herokuapp.com/api/characters/students";
 
 const getAllCharacters = async () => {
     try {
         const res = await fetch(API_URL);
         const characters = await res.json();
-        console.log(characters);
+        drawCards(characters);
 
     } catch (error) {
         console.log(error);
@@ -20,4 +17,18 @@ const getAllCharacters = async () => {
     } finally {
         console.log('End of Request');
     }
+}
+
+const drawCards = (characters) => {
+    const cards = document.querySelector("#cards");
+    const cardTemplate = document.querySelector("#cardTemplate");
+    const fragment = document.createDocumentFragment();
+
+    characters.forEach(item => {
+        const clone = cardTemplate.content.cloneNode(true); // Good practise is to clen the node
+        clone.querySelector("h5").textContent = item.name; // Select the element h5 only from the template
+        clone.querySelector("p").textContent = item.house + ' - ' + item.gender;
+        fragment.appendChild(clone)
+    });
+    cards.appendChild(fragment);
 }
